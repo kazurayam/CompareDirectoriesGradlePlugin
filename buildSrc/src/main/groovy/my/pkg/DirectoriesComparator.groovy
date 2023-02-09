7ada442d519183c13e43d06ebf9e0333fd035c6c
kazurayam <kazuaki.urayama@gmail.com>
2023-02-09 12:18:03 +0900

    save progress

--- buildSrc/src/main/groovy/my/pkg/DirectoriesComparator.groovy
+++ buildSrc/src/main/groovy/my/pkg/DirectoriesComparator.groovy
@@ -1,17 +1,27 @@
 package my.pkg
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileTree
+import org.gradle.api.file.ProjectLayout
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.file.FileCollection
+import java.nio.file.Paths
 
 abstract public class DirectoriesComparator extends DefaultTask {
 
     @Input
-    abstract public Property<String> getSourceDir();
+    abstract public Property<String> getSourceDir()
 
     @Input
-    abstract public Property<String> getTargetDir();
+    abstract public Property<String> getTargetDir()
+
+    @Input
+    abstract public Property<FileTree> getSourceFileTree()
+
+    @Input
+    abstract public Property<FileTree> getTargetFileTree()
 
     public DirectoriesComparator() {
         getSourceDir().convention(".")
@@ -20,7 +30,19 @@ abstract public class DirectoriesComparator extends DefaultTask {
 
     @TaskAction
     public void action() {
-        println("sourceDir: ${getSourceDir().get()}")
-        println("targetDir: ${getTargetDir().get()}")
+        println("sourceDir : ${getSourceDir().get()}")
+        println("targetDir : ${getTargetDir().get()}")
+        //
+        println("sourceFileTree: ${getSourceFileTree().get().toString()}")
+        FileTree sourceTree = getSourceFileTree().get()
+        sourceTree.each { File file ->
+            println("    " + file)
+        }
+        //
+        println("targetFileTree: ${getTargetFileTree().get().toString()}")
+        FileTree targetTree = getTargetFileTree().get()
+        targetTree.each {File file ->
+            println("    " + file)
+        }
     }
 }
--- buildSrc/src/main/groovy/my/pkg/DirectoriesComparatorPlugin.groovy
+++ buildSrc/src/main/groovy/my/pkg/DirectoriesComparatorPlugin.groovy
@@ -1,7 +1,7 @@
 package my.pkg;
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 public class DirectoriesComparatorPlugin implements Plugin<Project> {
     @Override
@@ -16,6 +16,10 @@ public class DirectoriesComparatorPlugin implements Plugin<Project> {
                 DirectoriesComparator.class, task -> {
             task.getSourceDir().set(extension.getSourceDir())
             task.getTargetDir().set(extension.getTargetDir())
+            task.getSourceFileTree().set(
+                    project.fileTree(extension.getSourceDir()))
+            task.getTargetFileTree().set(
+                    project.fileTree(extension.getTargetDir()))
         })
     }
 }
